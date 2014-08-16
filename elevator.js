ed27e25bee23f6954faa46d171ebdb78933a6afb
jgerardsimcock <justin_simcock@yahoo.com>
2014-08-16 10:52:21 -1000

    merged

--- elevator.js
+++ elevator.js
@@ -3,4 +3,28 @@
 
 module.exports = {
 
+  goto: function(currentLevel, endLevel){
+      
+
+      var endLevelToInteger = parseInt(endLevel);
+
+      if(endLevelToInteger < currentLevel) {
+        return endLevelToInteger - currentLevel;
+      }
+      if (-3 > endLevelToInteger > 3){
+        return currentLevel;
+      }
+      
+      return currentLevel + endLevelToInteger;
+
+
+
+ 
+
+  }
+
+
+
 };
+
+//we need a function that takes the current level and then adds/substracts the value of the argument
